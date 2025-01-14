f664d69f7de09b527f184b4b829493d4e9e134e4
Anslem Kelechi <anslem.okoronkwo@mykinetic.app>
2025-01-14 13:29:27 +0100

    feat(core): Proper Scalffording

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
@@ -7,19 +8,18 @@ import App from './App';
 // Setup Axios once here
 axios.defaults.headers = { Accept: 'application/json' };
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 export default axiosInstance;
 
-// Find all widget divs
-const widgetDivs = document.querySelectorAll('.nicoraynaud-finance-widget');
+// Define renderWidget function and attach it to the global window object
+window.renderWidget = function (div) {
+  const symbol = div.dataset.symbol;
 
-// Inject our React App into each class
-widgetDivs.forEach(div => {
-    ReactDOM.render(
-      <React.StrictMode>
-        <App symbol={div.dataset.symbol}/>
-      </React.StrictMode>,
-        div
-    );
-});
+  ReactDOM.render(
+    <React.StrictMode>
+      <App symbol={symbol} />
+    </React.StrictMode>,
+    div
+  );
+};
